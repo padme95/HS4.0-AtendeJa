64190f292e8ef6a95263ad7cba08ae2e1789b833
Bravo <manuelbravoc@gmail.com>
2024-06-21 20:10:20 -0300

    google maps

--- hs-atende-ja/src/pages/Agendamento.tsx
+++ hs-atende-ja/src/pages/Agendamento.tsx
@@ -1,11 +1,70 @@
-import React from 'react';
+// src/pages/Agendamento.tsx
+import React, { useState, useEffect } from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import "./Agendamento.css"
+
+const containerStyle = {
+  width: '100%',
+  height: '400px'
+};
+
+const center = {
+  lat: -23.55052,
+  lng: -46.633308
+};
+
+const markers = [
+  {
+    id: 1,
+    name: "Clinica Santo Amaro",
+    position: { lat: -23.5952366, lng: -46.6469491 }
+  },
+  {
+    id: 2,
+    name: "Rede Protege +",
+    position: { lat: -23.5632698, lng: -46.5945727 }
+  },
+  {
+    id: 3,
+    name: "Clinica Saúde Em Primeiro Lugar",
+    position: { lat: -23.5448216, lng: -46.5953351 }
+  }
+];
 
 const Agendamento: React.FC = () => {
+  const [userLocation, setUserLocation] = useState<google.maps.LatLngLiteral | null>(null);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          const { latitude, longitude } = position.coords;
+          setUserLocation({ lat: latitude, lng: longitude });
+        },
+        error => console.error(error)
+      );
+    }
+  }, []);
+
   return (
-    <div>
-      <h1>Agendamento</h1>
-      <div id="map"></div>
-    </div>
+    <LoadScript googleMapsApiKey="AIzaSyAkBw5ZyUeP0FYNWyhlREMJR-yb_yZcGFs">
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={userLocation || center}
+        zoom={10}
+      >
+        {userLocation && (
+          <Marker
+            position={userLocation}
+            label="Você está aqui"
+            icon="http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+          />
+        )}
+        {markers.map(marker => (
+          <Marker key={marker.id} position={marker.position} label={marker.name} />
+        ))}
+      </GoogleMap>
+    </LoadScript>
   );
 };
 
