b6da809ecf5e36a8814da6dca9c561b83cdff141
Bravo <manuelbravoc@gmail.com>
2024-06-21 20:03:36 -0300

    menu hamburguer

--- hs-atende-ja/src/App.tsx
+++ hs-atende-ja/src/App.tsx
@@ -1,30 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import BemVindo from './pages/BemVindo';
 import Perfil from './pages/Perfil';
 import Historico from './pages/Historico';
 import Agendamento from './pages/Agendamento';
 import Login from './pages/Login';
-import './App.css';
+import BemVindo from './pages/BemVindo';
 
 const App: React.FC = () => {
-  const isLoggedIn = !!localStorage.getItem('token');
-
   return (
     <Router>
-      <div>
-        {isLoggedIn && <Navbar />}
-        <div className="content">
-          <Routes>
-            {!isLoggedIn && <Route path="/" element={<Login />} />}
-            {isLoggedIn && <Route path="/" element={<BemVindo />} />}
-            {isLoggedIn && <Route path="/perfil" element={<Perfil />} />}
-            {isLoggedIn && <Route path="/historico" element={<Historico />} />}
-            {isLoggedIn && <Route path="/agendamento" element={<Agendamento />} />}
-            {!isLoggedIn && <Route path="*" element={<Login />} />}
-          </Routes>
-        </div>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<BemVindo />} />
+          <Route path="/perfil" element={<Perfil />} />
+          <Route path="/historico" element={<Historico />} />
+          <Route path="/agendamento" element={<Agendamento />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
       </div>
     </Router>
   );
--- hs-atende-ja/src/components/Navbar.tsx
+++ hs-atende-ja/src/components/Navbar.tsx
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className="navbar">
-      <ul>
-        <li><Link to="/">Bem-vindo</Link></li>
-        <li><Link to="/perfil">Perfil</Link></li>
-        <li><Link to="/historico">Histórico</Link></li>
-        <li><Link to="/agendamento">Agendamento</Link></li>
-        <li><Link to="/login">Login</Link></li>
-      </ul>
-    </nav>
+    <div className={`navbar-container ${isOpen ? 'menu-open' : ''}`}>
+      <div className="navbar-header">
+        <div className="navbar-toggle" onClick={toggleMenu}>
+          <div className={`hamburger ${isOpen ? 'open' : ''}`}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+        <div className="navbar-title">
+          <h1>Meu Aplicativo</h1>
+        </div>
+      </div>
+      <div className={`menu-container ${isOpen ? 'open' : ''}`}>
+        <nav className="navbar">
+          <ul>
+            <li><Link to="/" onClick={toggleMenu}>Bem-vindo</Link></li>
+            <li><Link to="/perfil" onClick={toggleMenu}>Perfil</Link></li>
+            <li><Link to="/historico" onClick={toggleMenu}>Histórico</Link></li>
+            <li><Link to="/agendamento" onClick={toggleMenu}>Agendamento</Link></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
   );
 };
 
